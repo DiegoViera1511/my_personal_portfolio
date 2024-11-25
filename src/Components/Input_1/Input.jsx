e605c7f6738c4a3083a5ff60f03476b8f4facb2c
Diego Viera <diegovieramartinez@gmail.com>
2024-11-24 22:25:21 -0500

    added eye revelation to password input

--- src/Components/Input_1/Input.jsx
+++ src/Components/Input_1/Input.jsx
@@ -1,17 +1,33 @@
 import "./input_1.css"
+import {useState} from "react";
 
-export function Input_1({input_id, input_type, input_value, input_onChange, input_required, input_placeholder}) {
+export function Input_1({input_id, input_type, input_value, input_onChange, input_required, input_placeholder , icon}) {
+    const [type , setType] = useState(input_type)
+    
     return (
         <div className="input_1_container">
             <input
                 className="input_1"
                 id={input_id}
-                type={input_type}
+                type={type}
                 value={input_value}
                 onChange={input_onChange}
                 required={input_required}
                 placeholder={input_placeholder}
             />
+            {input_type === "password" ?
+                <div className="input_icon type_password" 
+                     onMouseOver={() => setType("text")} 
+                     onMouseLeave={() => setType("password")}
+                >
+                    {input_type === "password" && <i className="uil uil-eye"></i>}
+                </div>
+                :
+                <div className="input_icon">
+                    <i className={icon}></i>
+                </div>
+            }
+
         </div>
     )
 
