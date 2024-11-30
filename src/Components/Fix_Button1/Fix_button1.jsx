cf7c0a641096555241a3c89a541403611c9a5214
Diego Viera <diegovieramartinez@gmail.com>
2024-11-30 11:32:12 -0500

    added fix_button1 for configuration button onportfolio

---
+++ src/Components/Fix_Button1/Fix_button1.jsx
@@ -0,0 +1,9 @@
+import "./fix_button1.css"
+
+export function Fix_button1({ icon }){
+    return (
+        <button className="fix_button1">
+            <i className={icon}></i>
+        </button>
+    )
+}
